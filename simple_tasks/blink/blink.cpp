22423c5aff552ad81b42c4546049bf9173424996
Devon Gardner <dgardner365@gmail.com>
2022-09-19 11:37:04 -0400

    reformatted styling and switched from led to groupLed commands

--- simple_tasks/blink/blink.cpp
+++ simple_tasks/blink/blink.cpp
@@ -1,55 +1,54 @@
 // For getting key press
-#include <unistd.h>
 #include <termios.h>
+#include <unistd.h>
 
-#include <iostream>
 #include <dynamixel_helper.h>
+#include <iostream>
 
 // Get character input
 int getch() {
-    struct termios oldt, newt; // old terminal mode, new terminal mode
-    int ch;
-    tcgetattr(STDIN_FILENO, &oldt); // Get old terminal mode from terminal STDIN_FILENO
-    newt = oldt; // Store old terminal mode in new terminal mode variable
-    newt.c_lflag &= ~(ICANON | ECHO); // Enter non-canonical mode (don't wait for enter key) and disable echoing.
-    tcsetattr(STDIN_FILENO, TCSANOW, &newt); // Set terminal to new mode
-    ch = getchar(); // Get character from terminal in new mode
-    tcsetattr(STDIN_FILENO, TCSANOW, &oldt); // Set terminal to old mode again
-    return ch;
+  struct termios oldt, newt; // old terminal mode, new terminal mode
+  int ch;
+  tcgetattr(STDIN_FILENO,
+            &oldt); // Get old terminal mode from terminal STDIN_FILENO
+  newt = oldt;      // Store old terminal mode in new terminal mode variable
+  newt.c_lflag &= ~(ICANON | ECHO); // Enter non-canonical mode (don't wait for
+                                    // enter key) and disable echoing.
+  tcsetattr(STDIN_FILENO, TCSANOW, &newt); // Set terminal to new mode
+  ch = getchar(); // Get character from terminal in new mode
+  tcsetattr(STDIN_FILENO, TCSANOW, &oldt); // Set terminal to old mode again
+  return ch;
 }
 
 int main() {
-    
-    const char port[] = "/dev/ttyUSB0";
-    DynamixelHelper dynamixelHelper(port);
 
-    uint8_t motorIDs[] = {1, 2, 4, 5, 6};
+  const char port[] = "/dev/ttyUSB0";
+  DynamixelHelper dh(port);
 
-    dynamixelHelper.openPort();
-    dynamixelHelper.setBaudrate(1000000);
+  vector<uint8_t> motorIDs{1, 2, 4, 5, 6};
 
-    // Blink all motor leds
-    bool led_status = false;
-    while(true) {
-        std::cout << "Press any key to toggle LEDs. (Press [ESC] to exit)" << std::endl;
-        if (getch() == 0x1b)
-          break;
+  dh.openPort();
+  dh.setBaudrate(1000000);
 
-        // Toggle led on/off
-        led_status = !led_status;
+  // Blink all motor leds
+  bool led_status = false;
+  while (true) {
+    std::cout << "Press any key to toggle LEDs. (Press [ESC] to exit)"
+              << std::endl;
+    if (getch() == 0x1b)
+      break;
 
-        for (uint8_t id : motorIDs)
-        {
-            if (led_status)
-                dynamixelHelper.ledEnable(id);            
-            else
-                dynamixelHelper.ledDisable(id);
-        }
-    }
+    // Toggle led on/off
+    led_status = !led_status;
 
-    // Disable all leds before stopping program
-    for (uint8_t id : motorIDs)
-        dynamixelHelper.ledDisable(id);
+    if (led_status)
+      dh.groupLedEnable(motorIDs);
+    else
+      dh.groupLedDisable(motorIDs);
+  }
 
-    return 0;
+  // Disable all leds before stopping program
+  dh.groupLedDisable(motorIDs);
+
+  return 0;
 }
