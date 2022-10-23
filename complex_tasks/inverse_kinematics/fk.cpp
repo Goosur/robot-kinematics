3925651794ca99eb10f664d2bcccd19586f9b4ca
Devon Gardner <dgardner365@gmail.com>
2022-10-23 14:20:33 -0400

    reimplemented forward kinematics in object oriented fashion

---
+++ complex_tasks/inverse_kinematics/fk.cpp
@@ -0,0 +1,53 @@
+#include "fk.h"
+
+/**
+ * Generate 2d array of Denavit-Hartenberg parameters from alphas, as, ds, and
+ * thetas.
+ */
+vector<array<double, 4>> FK::generate_parameters(const vector<double> &thetas) {
+  vector<array<double, 4>> parameters;
+  for (int i = 0; i < thetas.size(); i++)
+    parameters.push_back({this->alpha[i], this->a[i], this->d[i], thetas[i]});
+  return parameters;
+}
+
+/**
+ * Takes alpha_{i-1}, a_{i-1}, d_i, and theta_i in an array
+ * Creates Denavit-Hartenberg transformation matrix for give joint paramters
+ * Returns transformation matrix for joint i-1 to i
+ */
+Eigen::Matrix4d
+FK::generate_dh_transform(const array<double, 4> &joint_parameters) {
+  Eigen::Matrix4d T;
+  T << cos(joint_parameters[3]), -sin(joint_parameters[3]), 0.0,
+      joint_parameters[1], sin(joint_parameters[3]) * cos(joint_parameters[0]),
+      cos(joint_parameters[3]) * cos(joint_parameters[0]),
+      -sin(joint_parameters[0]),
+      -sin(joint_parameters[0]) * joint_parameters[2],
+      sin(joint_parameters[3]) * sin(joint_parameters[0]),
+      cos(joint_parameters[3]) * sin(joint_parameters[0]),
+      cos(joint_parameters[0]), cos(joint_parameters[0]) * joint_parameters[2],
+      0.0, 0.0, 0.0, 1.0;
+
+  return T;
+}
+
+/**
+ * Computes current 3d coordinates of end effector using current joint angles
+ */
+array<double, 3>
+FK::get_end_effector_coordinates(const vector<double> &joint_angles) {
+  // Get parameters for each joint
+  vector<array<double, 4>> parameters = this->generate_parameters(joint_angles);
+
+  // Start with transform from world to first join
+  Eigen::Matrix4d T_end_effector = this->generate_dh_transform(parameters[0]);
+  // Keep building transforms onto eachother like the book to get the transform
+  // from world to end effector
+  for (int i = 1; i < parameters.size(); i++)
+    T_end_effector *= this->generate_dh_transform(parameters[i]);
+
+  // Coordinates of end effector come from the third column of the final DH
+  // transform matrix
+  return {T_end_effector(0, 3), T_end_effector(1, 3), T_end_effector(2, 3)};
+}
---
+++ complex_tasks/inverse_kinematics/fk.h
@@ -0,0 +1,30 @@
+#ifndef FK_H
+#define FK_H
+
+#include <array>
+#include <eigen3/Eigen/Eigen>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+
+class FK {
+public:
+  FK(const vector<double> &alpha, const vector<double> &a,
+     const vector<double> &d)
+      : alpha(alpha), a(a), d(d) {}
+
+  array<double, 3>
+  get_end_effector_coordinates(const vector<double> &joint_angles);
+
+private:
+  vector<double> alpha;
+  vector<double> a;
+  vector<double> d;
+
+  vector<array<double, 4>> generate_parameters(const vector<double> &thetas);
+  Eigen::Matrix4d
+  generate_dh_transform(const array<double, 4> &joint_parameters);
+};
+
+#endif
