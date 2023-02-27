be8c765959365c319e174542bcc96cfa5815e98f
Devon Gardner <dgardner365@gmail.com>
2023-02-27 01:34:38 -0500

    Removed useless imports and reformatted

--- ros2/src/tf2_tutorial_cpp/src/static_turtle_tf2_broadcaster.cpp
+++ ros2/src/tf2_tutorial_cpp/src/static_turtle_tf2_broadcaster.cpp
@@ -1,6 +1,3 @@
-#include <memory>
-#include <iostream>
-#include <rclcpp/logging.hpp>
 #include "geometry_msgs/msg/transform_stamped.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "tf2/LinearMath/Quaternion.h"
@@ -8,8 +5,10 @@
 
 class StaticFramePublisher : public rclcpp::Node {
 public:
-  explicit StaticFramePublisher(char *transformation[]) : Node("static_turtle_tf2_broadcaster") {
-    tf_static_broadcaster_ = std::make_shared<tf2_ros::StaticTransformBroadcaster>(this);
+  explicit StaticFramePublisher(char *transformation[])
+      : Node("static_turtle_tf2_broadcaster") {
+    tf_static_broadcaster_ =
+        std::make_shared<tf2_ros::StaticTransformBroadcaster>(this);
 
     // Publish static transforms once at startup
     this->make_transforms(transformation);
@@ -27,10 +26,8 @@ private:
     t.transform.translation.x = atof(transformation[3]);
     t.transform.translation.x = atof(transformation[4]);
     tf2::Quaternion q;
-    q.setRPY(
-	     atof(transformation[5]),
-	     atof(transformation[6]),
-	     atof(transformation[7]));
+    q.setRPY(atof(transformation[5]), atof(transformation[6]),
+             atof(transformation[7]));
     t.transform.rotation.x = q.x();
     t.transform.rotation.y = q.y();
     t.transform.rotation.z = q.z();
@@ -39,16 +36,17 @@ private:
     tf_static_broadcaster_->sendTransform(t);
   }
   std::shared_ptr<tf2_ros::StaticTransformBroadcaster> tf_static_broadcaster_;
-}
+};
 
-  int main(int argc, char **argv) {
+int main(int argc, char **argv) {
   auto logger = rclcpp::get_logger("logger");
 
   // Obtain parameters from command line arguments
   if (argc != 8) {
-    RCLCPP_INFO(logger, "Invalid number of parameters\nusage: "
-		"$ ros2 run tf2_tutorial_cpp static_turtle_tf2_broadcaster "
-		"child_frame_name x y z roll pitch yaw");
+    RCLCPP_INFO(logger,
+                "Invalid number of parameters\nusage: "
+                "$ ros2 run tf2_tutorial_cpp static_turtle_tf2_broadcaster "
+                "child_frame_name x y z roll pitch yaw");
     return 1;
   }
   return 0;
