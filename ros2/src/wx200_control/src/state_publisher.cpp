e4f7707829c9e216414f00c1b024113eef680191
Devon Gardner <dgardner365@gmail.com>
2023-04-16 19:58:12 -0400

    added state publisher to launch file and tested publishing

--- ros2/src/wx200_control/src/state_publisher.cpp
+++ ros2/src/wx200_control/src/state_publisher.cpp
@@ -16,10 +16,17 @@ private:
   void timer_callback() {
     auto message = sensor_msgs::msg::JointState();
     message.header.stamp = this->get_clock()->now();
+    message.name = {"test", "joints"};
+    message.position = {0.5, 0.7};
     publisher->publish(message);
   }
   rclcpp::TimerBase::SharedPtr timer;
   rclcpp::Publisher<sensor_msgs::msg::JointState>::SharedPtr publisher;
 };
 
-int main(int argc, char **argv) { return 0; }
+int main(int argc, char **argv) {
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<StatePublisher>());
+  rclcpp::shutdown();
+  return 0;
+}
