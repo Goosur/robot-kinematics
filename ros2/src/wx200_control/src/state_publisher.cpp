422a9f16b49f48dddb1eacb2238887f52a4d99b9
Devon Gardner <dgardner365@gmail.com>
2023-04-16 15:54:53 -0400

    created publisher that will live update robot state

--- ros2/src/wx200_control/src/state_publisher.cpp
+++ ros2/src/wx200_control/src/state_publisher.cpp
@@ -1,13 +1,25 @@
 #include "rclcpp/rclcpp.hpp"
-#include "sensor_msgs/msg/multi_dof_joint_state.hpp"
-#include "geometry_msgs/msg/quaternion.hpp"
+#include "sensor_msgs/msg/joint_state.hpp"
+
+using namespace std::chrono_literals;
 
 class StatePublisher : public rclcpp::Node {
 public:
+  StatePublisher() : Node("state_publisher") {
+    this->publisher = this->create_publisher<sensor_msgs::msg::JointState>(
+        "joint_states", 10);
+    this->timer = this->create_wall_timer(
+        500ms, std::bind(&StatePublisher::timer_callback, this));
+  }
+
 private:
-    rclcpp::TimerBase::SharedPtr timer_;
+  void timer_callback() {
+    auto message = sensor_msgs::msg::JointState();
+    message.header.stamp = this->get_clock()->now();
+    publisher->publish(message);
+  }
+  rclcpp::TimerBase::SharedPtr timer;
+  rclcpp::Publisher<sensor_msgs::msg::JointState>::SharedPtr publisher;
 };
 
-int main(int argc, char **argv) {
-    return 0;
-}
+int main(int argc, char **argv) { return 0; }
